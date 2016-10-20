1dd6482d169d34e3fab5e4ef55c7a2aa578e5997
vlterno <vlterno@gmail.com>
2016-10-20 12:37:54 +0300

    initial commit (studing react)

---
+++ js/app.js
@@ -0,0 +1,36 @@
+var News = React.createClass({
+  render: function() {
+    return (
+      <div className="news">
+        К сожалению, новостей нет.
+      </div>
+    );
+  }
+});
+
+var Comments = React.createClass({
+  render: function() {
+    return (
+      <div className="comments">
+        Нет новостей - комментировать нечего
+      </div>
+    );
+  }
+});
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <div className="app">
+        Всем привет, я компонент App! Я умею отображать новости.
+        <News />
+        <Comments />
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
